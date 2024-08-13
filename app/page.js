eb880160be41eae36f14026192466b72d7fd9016
steve-rops <somebiss@gmail.com>
2024-08-13 22:29:25 +0300

    fixed responsiveness on mobile view

--- app/page.js
+++ app/page.js
@@ -5,11 +5,13 @@ import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
-    <div className="lg:container p-10 space-y-5">
-      <div className="container py-10 h-96 bg-no-repeat bg-center bg-contain  bg-[url('../public/cartoon-car.jpeg')] lg:items-start">
-        <div className=" bg-gray-50/65 w-[50%] mx-auto ">
-          <h2 className="lg:text-4xl">Renting a car was never easier.</h2>
-          <h3 className="text-xs lg:text-lg text-gray-800">
+    <div className="lg:container px-4 py-3 lg:p-10 space-y-1">
+      <div className="container py-10 h-96 bg-no-repeat bg-center bg-contain bg-[url('../public/cartoon-car.jpeg')] lg:items-start">
+        <div className=" bg-gray-50/65 md:w-[50%] p-2 translate-y-[30%]  mx-auto ">
+          <h2 className="lg:text-4xl text-2xl font-semibold">
+            Renting a car was never easier.
+          </h2>
+          <h3 className="lg:text-lg text-gray-800">
             Select one of our many cars
           </h3>
         </div>
